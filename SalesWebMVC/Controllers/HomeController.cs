d1d46c0ebd6027aa177f23c560faff8625e70f2e
Jean Carlo Vanzela Prado <jeanpradoart@gmail.com>
2020-05-15 21:33:00 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData["Student"] = "Jean Carlo Vanzela Prado";
 
             return View();
         }
