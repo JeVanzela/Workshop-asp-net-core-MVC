63bd1c805cfc30b80713a05e2144b91b577cc5cc
Jean Carlo Vanzela Prado <jeanpradoart@gmail.com>
2020-05-25 20:59:38 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,6 +9,29 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller se)
+        {
+            Sellers.Add(se);
+        }
+
+        public void RemoveSeller(Seller se)
+        {
+            Sellers.Remove(se);
+        }
+
+        public double TotalSellers(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(se => se.TotalSales(initial,final));
+        }
     }
 }
